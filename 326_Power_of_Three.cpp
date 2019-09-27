2c18f6de74f5f80c459f4221c3f3cd0c43504d67
chernovdmitrii <55793294+chernovdmitrii@users.noreply.github.com>
2019-09-27 20:32:17 +0300

    Create 326_Power_of_Three.cpp

---
+++ 326_Power_of_Three.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/power-of-three
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        while (n % 3 == 0)  n /= 3;
+        return (n == 1);
+    }
+};
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        int n = stringToInteger(line);
+        
+        bool ret = Solution().isPowerOfThree(n);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
