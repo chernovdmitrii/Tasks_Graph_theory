f5a456a83b6e8748b31f55b08129b2ba242a8db6
chernovdmitrii <55793294+chernovdmitrii@users.noreply.github.com>
2019-11-01 16:48:33 +0300

    Create 101_Symmetric_Tree.py

---
+++ 101_Symmetric_Tree.py
@@ -0,0 +1,77 @@
+# https://leetcode.com/problems/symmetric-tree/submissions/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        return self.isEqual(root, root)
+        
+    
+    def isEqual(self, root_1, root_2) -> bool:
+        if not root_1 and not root_2: 
+            return True
+        if (not root_1 or not root_2) or (root_2 and not root_1):
+            return False; 
+        if(root_1.val == root_2.val):
+            return self.isEqual(root_1.left, root_2.right ) and self.isEqual(root_1.right, root_2.left )
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            root = stringToTreeNode(line);
+            
+            ret = Solution().isSymmetric(root)
+
+            out = (ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
